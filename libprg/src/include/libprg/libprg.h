5b677d921b317e91e37fd845a7fe8492a6cbe87e
aluno <kaua.vc06@aluno.ifsc.edu.br>
2025-09-02 11:16:59 -0300

    teste

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,6 +1,19 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
-typedef struct pilha pilha_t;
-pilha_t* criar_pilha;
+typedef struct pilha {
+    int* elementos;
+    int topo;
+    int capacidade;
+} pilha_t;
+pilha_t* criar_pilha(int capacidade);
+int empilhar(pilha_t* p, int valor);
+int desempilhar(pilha_t* p, int* valor);
+int topo_pilha(pilha_t* p, int* valor);
+int tamanho_pilha(pilha_t* p);
+void imprimir_pilha(pilha_t* p);
+void destruir_pilha(pilha_t* p);
+
 #endif
+
+
